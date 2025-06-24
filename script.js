1f4f5747aac934d75d7a1c5d05a51cb812668009
oancholarevelo <oancholarevelo@gmail.com>
2025-06-24 11:37:41 +0800

    .

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ darkModeButton.addEventListener('click', function() {
 
 // Hamburger menu toggle
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".nav-bar");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    navBar.classList.toggle("active");
 })
